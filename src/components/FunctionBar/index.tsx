bd3b1e74abd3153b954031b77204d08aa8d6231d
PhuongThan25503 <phuongthan25503@gmail.com>
2024-06-10 00:16:14 +0700

    oke

--- src/components/FunctionBar/index.tsx
+++ src/components/FunctionBar/index.tsx
@@ -1,12 +1,16 @@
 import { Images } from "@/images";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 type FunctionBarProp = {};
 export const FunctionBar = ({}: FunctionBarProp): React.JSX.Element => {
   return (
     <div className="fixed flex bottom-0 bg-appWhite">
-      <button className="flex border-t-2 border-r-2 border-l-2 p-2 items-center rounded-t-3xl">
+      <Link
+        href={"https://www.facebook.com/happypetals.giftstore/"}
+        className="flex border-t-2 border-r-2 border-l-2 p-2 items-center rounded-t-3xl"
+      >
         <Image
           width={50}
           height={50}
@@ -14,7 +18,7 @@ export const FunctionBar = ({}: FunctionBarProp): React.JSX.Element => {
           alt="messengericon"
         />
         <p>Liên hệ đặt hàng</p>
-      </button>
+      </Link>
     </div>
   );
 };
